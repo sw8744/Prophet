4e2c99e7978630115596ab96754f7b07011b005a
Jasgo <70329768+ISHS-Jasgo@users.noreply.github.com>
2023-08-24 22:27:17 +0900

    add main method

--- predict_server.py
+++ predict_server.py
@@ -12,3 +12,7 @@ def hello():
 @app.route("/predict/<str:place>")
 def predict(place: str):
     return pcp.predict(place.replace("+", " "))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
