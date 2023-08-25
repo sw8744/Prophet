62581b8bb0d6778bf6b514e265320439bbc7b8ad
Jasgo <70329768+ISHS-Jasgo@users.noreply.github.com>
2023-08-25 23:24:33 +0900

    add cors

--- predict_server.py
+++ predict_server.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 import people_count_predict as pcp
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
